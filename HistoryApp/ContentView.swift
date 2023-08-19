a513a2a1e020c4843849f9a25a294e7d31c48b72
Elaidzha1940 <eli080293@gmail.com>
2023-08-19 12:12:43 +0300

    HistoryApp

---
+++ HistoryApp/ContentView.swift
@@ -0,0 +1,30 @@
+//  /*
+//
+//  Project: HistoryApp
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 19.08.2023
+//
+//  Satatus: in progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HistoryApp/HistoryAppApp.swift
@@ -0,0 +1,21 @@
+//  /*
+//
+//  Project: HistoryApp
+//  File: HistoryAppApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 19.08.2023
+//
+//   Satatus: in progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+@main
+struct HistoryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
