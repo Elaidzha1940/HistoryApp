8c51b63efb7b0e0537a6eaabe127a9f5b40da78a
Elaidzha1940 <eli080293@gmail.com>
2023-08-19 12:29:41 +0300

    HistoryApp + added Images

--- HistoryApp/ContentView.swift
+++ HistoryApp/ContentView.swift
@@ -12,14 +12,13 @@
 import SwiftUI
 
 struct ContentView: View {
+        
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+          
+            
         }
-        .padding()
     }
 }
 
---
+++ HistoryApp/HistoricalArtifact.swift
@@ -0,0 +1,15 @@
+//  /*
+//
+//  Project: HistoryApp
+//  FHistoricalArtifact.swift
+//
+//
+//  Created by Elaidzha Shchukin on 19.08.2023.
+//
+
+import SwiftUI
+
+struct HistoricalArtifact: Identifiable {
+    
+}
+
